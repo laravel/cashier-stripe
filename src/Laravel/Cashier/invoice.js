36e4285b77a94427acc9a53784299a8b6c07e06c
Joost Koehoorn <JoostK@users.noreply.github.com>
2014-03-04 12:48:44 +0100

    Fix orientation in invoice.js
    Small typo fixed

--- src/Laravel/Cashier/invoice.js
+++ src/Laravel/Cashier/invoice.js
@@ -6,7 +6,7 @@ var fs = require('fs'),
 // Read the HTML view into the page...
 page.content = fs.read(args[1]);
 page.viewportSize = {width: 600, height: 600};
-page.paperSize = {format: 'A4', orientation: 'protrait', margin: '1cm'};
+page.paperSize = {format: 'A4', orientation: 'portrait', margin: '1cm'};
 
 // Give the page 250ms to render any images...
 window.setTimeout(function() {
