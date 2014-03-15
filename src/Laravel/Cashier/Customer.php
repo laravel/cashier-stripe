31692e638f47a896e4655552b49b7d1ac02cb9da
Taylor Otwell <taylorotwell@gmail.com>
2014-03-14 23:23:29 -0500

    Fix data.

--- src/Laravel/Cashier/Customer.php
+++ src/Laravel/Cashier/Customer.php
@@ -38,7 +38,7 @@ class Customer extends Stripe_Customer {
 	 */
 	public function findSubscription($id)
 	{
-		foreach ($this->subscriptions->all() as $subscription)
+		foreach ($this->subscriptions->all()->data as $subscription)
 		{
 			if ($subscription->id == $id) return $subscription;
 		}
