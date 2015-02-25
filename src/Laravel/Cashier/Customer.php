b17a86e22de95baea172664d03c52673cf2ae247
Taylor Otwell <taylorotwell@gmail.com>
2015-02-25 09:46:15 -0600

    Convert to PSR-2.

--- src/Laravel/Cashier/Customer.php
+++ src/Laravel/Cashier/Customer.php
@@ -3,103 +3,100 @@
 use Stripe_Customer;
 use Stripe_Subscription;
 
-class Customer extends Stripe_Customer {
+class Customer extends Stripe_Customer
+{
 
-	/**
-	 * The subscription being managed by Cashier.
-	 *
-	 * @var \Stripe_Subscription
-	 */
-	public $subscription;
+    /**
+     * The subscription being managed by Cashier.
+     *
+     * @var \Stripe_Subscription
+     */
+    public $subscription;
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public static function retrieve($id, $apiKey = null)
-	{
-		return self::_scopedRetrieve(get_called_class(), $id, $apiKey);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public static function retrieve($id, $apiKey = null)
+    {
+        return self::_scopedRetrieve(get_called_class(), $id, $apiKey);
+    }
 
-	/**
-	 * Get the current subscription ID.
-	 *
-	 * @return string|null
-	 */
-	public function getStripeSubscription()
-	{
-		return $this->subscription ? $this->subscription->id : null;
-	}
+    /**
+     * Get the current subscription ID.
+     *
+     * @return string|null
+     */
+    public function getStripeSubscription()
+    {
+        return $this->subscription ? $this->subscription->id : null;
+    }
 
-	/**
-	 * Find a subscription by ID.
-	 *
-	 * @param  string  $id
-	 * @return \Stripe_Subscription|null
-	 */
-	public function findSubscription($id)
-	{
-		foreach ($this->subscriptions->all()->data as $subscription)
-		{
-			if ($subscription->id == $id) return $subscription;
-		}
-	}
+    /**
+     * Find a subscription by ID.
+     *
+     * @param  string  $id
+     * @return \Stripe_Subscription|null
+     */
+    public function findSubscription($id)
+    {
+        foreach ($this->subscriptions->all()->data as $subscription) {
+            if ($subscription->id == $id) {
+                return $subscription;
+            }
+        }
+    }
 
-	/**
-	 * Create the current subscription with the given data.
-	 *
-	 * @param  array  $params
-	 * @return void
-	 */
-	protected function createSubscription(array $params)
-	{
-		return $this->subscription = $this->subscriptions->create($params);
-	}
+    /**
+     * Create the current subscription with the given data.
+     *
+     * @param  array  $params
+     * @return void
+     */
+    protected function createSubscription(array $params)
+    {
+        return $this->subscription = $this->subscriptions->create($params);
+    }
 
-	/**
-	 * Update the current subscription with the given data.
-	 *
-	 * @param  array  $params
-	 * @return \Stripe_Subscription
-	 */
-	public function updateSubscription($params = null)
-	{
-		if (is_null($this->subscription))
-		{
-			return $this->createSubscription($params);
-		}
-		else
-		{
-			return $this->saveSubscription($params);
-		}
-	}
+    /**
+     * Update the current subscription with the given data.
+     *
+     * @param  array  $params
+     * @return \Stripe_Subscription
+     */
+    public function updateSubscription($params = null)
+    {
+        if (is_null($this->subscription)) {
+            return $this->createSubscription($params);
+        } else {
+            return $this->saveSubscription($params);
+        }
+    }
 
-	/**
-	 * Save the current subscription with the given parameters.
-	 *
-	 * @param  array  $params
-	 * @return \Stripe_Subscription
-	 */
-	protected function saveSubscription($params)
-	{
-		foreach ($params as $key => $value)
-		{
-			$this->subscription->{$key} = $value;
-		}
+    /**
+     * Save the current subscription with the given parameters.
+     *
+     * @param  array  $params
+     * @return \Stripe_Subscription
+     */
+    protected function saveSubscription($params)
+    {
+        foreach ($params as $key => $value) {
+            $this->subscription->{$key} = $value;
+        }
 
-		$this->subscription->save();
+        $this->subscription->save();
 
-		return $this->subscription;
-	}
+        return $this->subscription;
+    }
 
-	/**
-	 * Cancel the current subscription.
-	 *
-	 * @param  array  $params
-	 * @return void
-	 */
-	public function cancelSubscription($params = null)
-	{
-		return $this->subscription->cancel($params);
-	}
-
+    /**
+     * Cancel the current subscription.
+     *
+     * @param  array  $params
+     * @return void
+     */
+    public function cancelSubscription($params = null)
+    {
+        return $this->subscription->cancel($params);
+    }
 }
--- src/Laravel/Cashier/EloquentBillableRepository.php
+++ src/Laravel/Cashier/EloquentBillableRepository.php
@@ -3,37 +3,36 @@
 use Illuminate\Support\Facades\Config;
 use Laravel\Cashier\Contracts\Billable as BillableContract;
 
-class EloquentBillableRepository implements BillableRepositoryInterface {
+class EloquentBillableRepository implements BillableRepositoryInterface
+{
 
-	/**
-	 * Find a Billable implementation by Stripe ID.
-	 *
-	 * @param  string  $stripeId
-	 * @return \Laravel\Cashier\Contracts\Billable
-	 */
-	public function find($stripeId)
-	{
-		$model = $this->createCashierModel(Config::get('services.stripe.model'));
+    /**
+     * Find a Billable implementation by Stripe ID.
+     *
+     * @param  string  $stripeId
+     * @return \Laravel\Cashier\Contracts\Billable
+     */
+    public function find($stripeId)
+    {
+        $model = $this->createCashierModel(Config::get('services.stripe.model'));
 
-		return $model->where($model->getStripeIdName(), $stripeId)->first();
-	}
+        return $model->where($model->getStripeIdName(), $stripeId)->first();
+    }
 
-	/**
-	 * Create a new instance of the Auth model.
-	 *
-	 * @param  string  $class
-	 * @return \Laravel\Cashier\Contracts\Billable
-	 */
-	protected function createCashierModel($class)
-	{
-		$model = new $class;
+    /**
+     * Create a new instance of the Auth model.
+     *
+     * @param  string  $class
+     * @return \Laravel\Cashier\Contracts\Billable
+     */
+    protected function createCashierModel($class)
+    {
+        $model = new $class;
 
-		if ( ! $model instanceof BillableContract)
-		{
-			throw new \InvalidArgumentException("Model does not implement Billable.");
-		}
+        if (! $model instanceof BillableContract) {
+            throw new \InvalidArgumentException("Model does not implement Billable.");
+        }
 
-		return $model;
-	}
-
+        return $model;
+    }
 }
