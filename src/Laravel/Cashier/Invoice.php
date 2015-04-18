ff855f4e84f731db6b4a12f030bce25cc2a380c5
David Bowen <david@sorora.co.uk>
2015-04-18 19:03:35 +0100

    Discount can't be null if it has a discount

--- src/Laravel/Cashier/Invoice.php
+++ src/Laravel/Cashier/Invoice.php
@@ -139,7 +139,7 @@ class Invoice
      */
     public function hasDiscount()
     {
-        return $this->subtotal > 0 && $this->subtotal != $this->total;
+        return $this->subtotal > 0 && $this->subtotal != $this->total && ! is_null($this->discount);
     }
 
     /**
