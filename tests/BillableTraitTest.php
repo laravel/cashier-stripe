8bb7221d7d803bbc809a29a1bdc21ca5098a73f9
Taylor Otwell <taylorotwell@gmail.com>
2015-11-11 08:43:45 -0600

    fix broken tests

--- tests/BillableTraitTest.php
+++ tests/BillableTraitTest.php
@@ -33,7 +33,7 @@ class BillableTraitTest extends PHPUnit_Framework_TestCase
     {
         $billable = m::mock('BillableTraitTestStub[findInvoice]');
         $billable->shouldReceive('findInvoice')->once()->with('id')->andReturn($invoice = m::mock('StdClass'));
-        $invoice->shouldReceive('download')->once()->with(['foo']);
+        $invoice->shouldReceive('download')->once()->with(['foo'], null);
 
         $billable->downloadInvoice('id', ['foo']);
     }
