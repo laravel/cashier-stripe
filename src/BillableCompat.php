3bb4c88059659eadd6cc76975b6a5534de76f112
Chris Morrell <inxilpro@users.noreply.github.com>
2017-07-03 19:31:12 -0400

    More compat

--- src/BillableCompat.php
+++ src/BillableCompat.php
@@ -4,6 +4,12 @@ namespace Laravel\Cashier;
 
 use Laravel\Cashier\Gateway\Stripe\Gateway as StripeGateway;
 
+/**
+ * Trait BillableCompat
+ *
+ * @package Laravel\Cashier
+ * @mixin \Laravel\Cashier\Billable
+ */
 trait BillableCompat
 {
     /**
@@ -38,6 +44,28 @@ trait BillableCompat
     }
 
     /**
+     * Create a Stripe customer for the given Stripe model.
+     *
+     * @param  string  $token
+     * @param  array  $options
+     * @return \Stripe\Customer
+     */
+    public function createAsStripeCustomer($token, array $options = [])
+    {
+        return $this->createAsCustomer('stripe', $token, $options);
+    }
+
+    /**
+     * Get the Stripe customer for the Stripe model.
+     *
+     * @return \Stripe\Customer
+     */
+    public function asStripeCustomer()
+    {
+        return $this->asCustomer('stripe');
+    }
+
+    /**
      * Determine if the entity has a Braintree customer ID.
      *
      * @return bool
@@ -45,5 +73,28 @@ trait BillableCompat
     public function hasBraintreeId()
     {
         return ! is_null($this->braintree_id);
+    }
+
+    /**
+     * Create a Braintree customer for the given model.
+     *
+     * @param  string  $token
+     * @param  array  $options
+     * @return \Braintree\Customer
+     * @throws \Exception
+     */
+    public function createAsBraintreeCustomer($token, array $options = [])
+    {
+        return $this->createAsCustomer('braintree', $token, $options);
+    }
+
+    /**
+     * Get the Braintree customer for the model.
+     *
+     * @return \Braintree\Customer
+     */
+    public function asBraintreeCustomer()
+    {
+        return $this->asCustomer('braintree');
     }
 }
