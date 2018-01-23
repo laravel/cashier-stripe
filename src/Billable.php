70c281c2343793a6bc646a36554ab7e7876b1a4f
Tom Planer <tplaner@gmail.com>
2018-01-22 21:30:45 -0500

    Prevents unauthorized access to Stripe invoices

--- src/Billable.php
+++ src/Billable.php
@@ -15,6 +15,7 @@ use Stripe\Customer as StripeCustomer;
 use Stripe\BankAccount as StripeBankAccount;
 use Stripe\InvoiceItem as StripeInvoiceItem;
 use Stripe\Error\InvalidRequest as StripeErrorInvalidRequest;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 trait Billable
@@ -282,6 +283,10 @@ trait Billable
             throw new NotFoundHttpException;
         }
 
+        if ($invoice->customer !== $this->stripe_id) {
+            throw new AccessDeniedHttpException;
+        }
+
         return $invoice;
     }
 
@@ -468,7 +473,7 @@ trait Billable
         $this->cards()->each(function ($card) {
             $card->delete();
         });
-        
+
         $this->updateCardFromStripe();
     }
 
