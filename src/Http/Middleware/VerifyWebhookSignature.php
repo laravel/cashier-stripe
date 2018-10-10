2265202c55d41dd592388060f184f50e70888507
Michael Cordingley <Michael.Cordingley@gmail.com>
2018-10-10 06:50:40 -0400

    Add VerifyWebhookSignature middleware.

---
+++ src/Http/Middleware/VerifyWebhookSignature.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Laravel\Cashier\Http\Middleware;
+
+use Closure;
+use Stripe\Error\SignatureVerification;
+use Stripe\WebhookSignature;
+
+final class VerifyWebhookSignature
+{
+    public function handle($request, Closure $next)
+    {
+        try {
+            WebhookSignature::verifyHeader(
+                $request->getContent(),
+                $request->header('Stripe-Signature'),
+                config('services.stripe.webhook.secret'),
+                config('services.stripe.webhook.tolerance', 300)
+            );
+        } catch (SignatureVerification $exception) {
+            return abort(403);
+        }
+
+        return $next($request);
+    }
+}
---
+++ tests/VerifyWebhookSignatureTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Laravel\Cashier\Tests;
+
+use Illuminate\Http\Request;
+use Laravel\Cashier\Http\Middleware\VerifyWebhookSignature;
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+final class VerifyWebhookSignatureTest extends PHPUnit_Framework_TestCase
+{
+    public function test_signature_checks_out()
+    {
+        $secret = 'secret';
+
+        config(['services.stripe.webhook.secret' => $secret]);
+
+        $request = new Request([], [], [], [], [], [], 'Signed Body');
+        $request->headers->set('Stripe-Signature', 't=' . time() . ',v1=' . $this->sign($request->getContent(), $secret));
+
+        $called = false;
+
+        (new VerifyWebhookSignature)->handle($request, function ($request) use (&$called) {
+            $called = true;
+        });
+
+        static::assertTrue($called);
+    }
+
+    private function sign($payload, $secret)
+    {
+        return hash_hmac("sha256", $payload, $secret);
+    }
+
+    public function test_bad_signature_aborts()
+    {
+        $secret = 'secret';
+
+        config(['services.stripe.webhook.secret' => $secret]);
+
+        $request = new Request([], [], [], [], [], [], 'Signed Body');
+        $request->headers->set('Stripe-Signature', 't=' . time() . ',v1=fail');
+
+        static::expectException(HttpException::class);
+
+        (new VerifyWebhookSignature)->handle($request, function ($request) {});
+    }
+
+    public function test_no_secret_aborts()
+    {
+        $secret = 'secret';
+
+        config(['services.stripe.webhook.secret' => '']);
+
+        $request = new Request([], [], [], [], [], [], 'Signed Body');
+        $request->headers->set('Stripe-Signature', 't=' . time() . ',v1=' . $this->sign($request->getContent(), $secret));
+
+        static::expectException(HttpException::class);
+
+        (new VerifyWebhookSignature)->handle($request, function ($request) {});
+    }
+}
