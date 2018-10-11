d450aa31f1b5e9f9618088ebe9fa4c2d77962d5d
Taylor Otwell <taylor@laravel.com>
2018-10-11 10:18:37 -0500

    formatting

--- src/Subscription.php
+++ src/Subscription.php
@@ -380,11 +380,11 @@ class Subscription extends Model
     }
 
     /**
-     * Syncs the tax percentage of user to the subscription.
+     * Sync the tax percentage of the user to the subscription.
      *
      * @return void
      */
-    public function updateTax()
+    public function syncTaxPercentage()
     {
         $subscription = $this->asStripeSubscription();
 
