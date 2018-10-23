be0bbad876b7510e39a7d29bc6ce9cff7a3979e4
Taylor Otwell <taylor@laravel.com>
2018-10-23 12:12:51 -0500

    code formatting

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -37,6 +37,55 @@ class WebhookController extends Controller
     }
 
     /**
+     * Handle customer subscription updated.
+     *
+     * @param  array $payload
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function handleCustomerSubscriptionUpdated(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        if ($user) {
+            $data = $payload['data']['object'];
+
+            $user->subscriptions->filter(function (Subscription $subscription) use ($data) {
+                return $subscription->stripe_id === $data['id'];
+            })->each(function (Subscription $subscription) use ($data) {
+                // Quantity...
+                if (isset($data['quantity'])) {
+                    $subscription->quantity = $data['quantity'];
+                }
+
+                // Plan...
+                if (isset($data['plan']['id'])) {
+                    $subscription->stripe_plan = $data['plan']['id'];
+                }
+
+                // Trial ending date...
+                if (isset($data['trial_end'])) {
+                    $trial_ends = Carbon::createFromTimestamp($data['trial_end']);
+
+                    if (! $subscription->trial_ends_at || $subscription->trial_ends_at->ne($trial_ends)) {
+                        $subscription->trial_ends_at = $trial_ends;
+                    }
+                }
+
+                // Cancellation date...
+                if (isset($data['cancel_at_period_end']) && $data['cancel_at_period_end']) {
+                    $subscription->ends_at = $subscription->onTrial()
+                                ? $subscription->trial_ends_at
+                                : Carbon::createFromTimestamp($data['current_period_end']);
+                }
+
+                $subscription->save();
+            });
+        }
+
+        return new Response('Webhook Handled', 200);
+    }
+
+    /**
      * Handle a cancelled customer from a Stripe subscription.
      *
      * @param  array  $payload
@@ -58,85 +107,6 @@ class WebhookController extends Controller
     }
 
     /**
-     * Handle deleted customer.
-     *
-     * @param  array $payload
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function handleCustomerDeleted(array $payload)
-    {
-        $user = $this->getUserByStripeId($payload['data']['object']['id']);
-
-        if ($user) {
-            $user->subscriptions->each(function (Subscription $subscription) {
-                $subscription->skipTrial()
-                    ->markAsCancelled();
-            });
-
-            $user->forceFill([
-                'card_brand' => null,
-                'card_last_four' => null,
-                'trial_ends_at' => null,
-                'stripe_id' => null,
-            ])->save();
-        }
-
-        return new Response('Webhook Handled', 200);
-    }
-
-    /**
-     * Handle customer subscription updated.
-     *
-     * @param  array $payload
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function handleCustomerSubscriptionUpdated(array $payload)
-    {
-        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
-
-        if ($user) {
-            $data = $payload['data']['object'];
-
-            $user->subscriptions->filter(function (Subscription $subscription) use ($data) {
-                return $subscription->stripe_id === $data['id'];
-            })->each(function (Subscription $subscription) use ($data) {
-
-                // Quantity
-                if (isset($data['quantity'])) {
-                    $subscription->quantity = $data['quantity'];
-                }
-
-                // Plan
-                if (isset($data['plan']['id'])) {
-                    $subscription->stripe_plan = $data['plan']['id'];
-                }
-
-                // Trial ends
-                if (isset($data['trial_end'])) {
-                    $trial_ends = Carbon::createFromTimestamp($data['trial_end']);
-
-                    if (! $subscription->trial_ends_at || $subscription->trial_ends_at->ne($trial_ends)) {
-                        $subscription->trial_ends_at = $trial_ends;
-                    }
-                }
-
-                // Cancellation
-                if (isset($data['cancel_at_period_end']) && $data['cancel_at_period_end']) {
-                    if ($subscription->onTrial()) {
-                        $subscription->ends_at = $subscription->trial_ends_at;
-                    } else {
-                        $subscription->ends_at = Carbon::createFromTimestamp($data['current_period_end']);
-                    }
-                }
-
-                $subscription->save();
-            });
-        }
-
-        return new Response('Webhook Handled', 200);
-    }
-
-    /**
      * Handle customer updated.
      *
      * @param  array $payload
@@ -144,30 +114,8 @@ class WebhookController extends Controller
      */
     protected function handleCustomerUpdated(array $payload)
     {
-        $user = $this->getUserByStripeId($payload['data']['object']['id']);
-
-        if ($user) {
-            $data = $payload['data']['object'];
-
-            // Change card details
-            if (isset(
-                $data['default_source'],
-                $payload['data']['previous_attributes']['default_source'],
-                $data['sources']['data']
-            )) {
-                $default_card = $data['default_source'];
-
-                foreach ($data['sources']['data'] as $card) {
-                    if (! isset($card['id']) || $card['id'] != $default_card) {
-                        continue;
-                    }
-
-                    $user->forceFill([
-                        'card_brand' => $card['brand'] ?? null,
-                        'card_last_four' => $card['last4'] ?? null,
-                    ])->save();
-                }
-            }
+        if ($user = $this->getUserByStripeId($payload['data']['object']['id'])) {
+            $user->updateCardFromStripe();
         }
 
         return new Response('Webhook Handled', 200);
@@ -181,17 +129,34 @@ class WebhookController extends Controller
      */
     protected function handleCustomerSourceDeleted(array $payload)
     {
-        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+        if ($user = $this->getUserByStripeId($payload['data']['object']['customer'])) {
+            $user->updateCardFromStripe();
+        }
+
+        return new Response('Webhook Handled', 200);
+    }
+
+    /**
+     * Handle deleted customer.
+     *
+     * @param  array $payload
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function handleCustomerDeleted(array $payload)
+    {
+        $user = $this->getUserByStripeId($payload['data']['object']['id']);
 
         if ($user) {
-            $data = $payload['data']['object'];
+            $user->subscriptions->each(function (Subscription $subscription) {
+                $subscription->skipTrial()->markAsCancelled();
+            });
 
-            if ($user->card_brand == $data['brand'] && $user->card_last_four == $data['last4']) {
-                $user->forceFill([
-                    'card_brand' => null,
-                    'card_last_four' => null,
-                ])->save();
-            }
+            $user->forceFill([
+                'stripe_id' => null,
+                'trial_ends_at' => null,
+                'card_brand' => null,
+                'card_last_four' => null,
+            ])->save();
         }
 
         return new Response('Webhook Handled', 200);
