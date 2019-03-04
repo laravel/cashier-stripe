5c5c309eb53bc8fd8577b25e19d8ff4a8b8f6230
Hafez Divandari <hafezdivandari@gmail.com>
2019-03-04 19:05:15 +0330

    Do not clear trial_ends_at value to NULL

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -159,7 +159,6 @@ class WebhookController extends Controller
 
             $user->forceFill([
                 'stripe_id' => null,
-                'trial_ends_at' => null,
                 'card_brand' => null,
                 'card_last_four' => null,
             ])->save();
