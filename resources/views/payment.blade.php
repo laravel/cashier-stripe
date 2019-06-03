2aa396c75ac078508568168d0e06653f585bb838
Dries Vints <dries.vints@gmail.com>
2019-06-03 13:34:55 +0200

    Remove < from back button

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -43,7 +43,7 @@
 
                 <a href="{{ $redirect ?? url('/') }}"
                    class="inline-block w-full px-4 py-3 bg-gray-200 hover:bg-gray-300 text-center text-gray-800 font-bold rounded">
-                    < {{ __('Back') }}
+                    {{ __('Back') }}
                 </a>
 
                 <script>
