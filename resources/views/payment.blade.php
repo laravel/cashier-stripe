e5e2362b6b368cd6de2d839cab776c1de60c0bd5
Dries Vints <dries.vints@gmail.com>
2019-06-03 12:54:32 +0200

    Design payment page

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -9,19 +9,14 @@
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
     <script src="https://js.stripe.com/v3"></script>
-
-    <style>
-        #card-element {
-            margin-bottom: 1.5rem;
-            padding: 1rem 1.5rem;
-        }
-    </style>
 </head>
-<body class="font-sans bg-gray-100 leading-normal p-4 h-full">
-    <div class="h-full flex justify-center items-center">
-        <div class="w-auto">
-            <div class="max-w-lg bg-white rounded-lg shadow-xl p-6 mb-4">
-                <h1 class="text-3xl mb-4 font-bold text-center">
+<body class="font-sans text-gray-700 bg-gray-100 leading-normal p-4 h-full">
+    <div class="h-full md:flex md:justify-center md:items-center">
+        <div class="w-full max-w-lg">
+            <p id="message" class="hidden mb-4 bg-red-400 px-6 py-4 rounded text-white"></p>
+
+            <div class="bg-white rounded-lg shadow-xl p-4 pt-6 mb-5">
+                <h1 class="text-2xl mb-4 font-bold text-center">
                     {{ __('Payment Confirmation') }}
                 </h1>
 
@@ -31,55 +26,71 @@
                     <p class="mb-4">{{ __('The payment was cancelled.') }}</p>
                 @else
                     <div id="payment-elements">
-                        <p class="mb-6">{{ __('Extra confirmation is needed to process your payment. Please confirm your payment by filling out your payment details below.') }}</p>
+                        <p class="mb-4">
+                            {{ __('Extra confirmation is needed to process your payment. Please confirm your payment by filling out your payment details below.') }}
+                        </p>
 
-                        <input id="cardholder-name" type="text" placeholder="{{ __('Name') }}"
-                            class="inline-block w-full px-6 py-4 mb-6 bg-gray-100 rounded">
-                        <div id="card-element"></div>
+                        <input id="cardholder-name" type="text" placeholder="{{ __('Name') }}" required
+                               class="inline-block w-full px-6 py-4 mb-4 bg-gray-100 rounded">
 
-                        <button id="card-button" class="inline-block w-full px-6 py-4 bg-blue-600 text-white font-bold text-lg rounded">
-                            {{ __('Submit Payment') }}
+                        <div id="card-element" class="bg-gray-100 rounded px-6 py-4 mb-4"></div>
+
+                        <button id="card-button" class="inline-block w-full px-4 py-3 mb-4 bg-blue-600 text-white font-bold rounded hover:bg-blue-500">
+                            {{ __('Confirm Payment') }}
                         </button>
                     </div>
-
-                    <p id="message" class="mb-4"></p>
-
-                    <script>
-                        const paymentElements = document.getElementById('payment-elements');
-                        const cardholderName = document.getElementById('cardholder-name');
-                        const cardButton = document.getElementById('card-button');
-                        const message = document.getElementById('message');
-
-                        const stripe = Stripe('{{ $stripeKey }}');
-                        const elements = stripe.elements();
-                        const cardElement = elements.create('card');
-                        cardElement.mount('#card-element');
-
-                        cardButton.addEventListener('click', function() {
-                            stripe.handleCardPayment(
-                                '{{ $payment->clientSecret() }}', cardElement, {
-                                    payment_method_data: {
-                                        billing_details: { name: cardholderName.value }
-                                    }
-                                }
-                            ).then(function (result) {
-                                if (result.error) {
-                                    message.innerText = 'Error: '+result.error.message;
-                                } else {
-                                    paymentElements.style.display = 'none';
-
-                                    message.innerText = '{{ __('The payment was successful.') }}';
-                                }
-                            });
-                        });
-                    </script>
                 @endif
-            </div>
 
-            <p class="text-center">
-                <a href="{{ $redirect ?? url('/') }}" class="text-blue-600">
+                <a href="{{ $redirect ?? url('/') }}"
+                   class="inline-block w-full px-4 py-3 bg-gray-200 hover:bg-gray-300 text-center text-gray-800 font-bold rounded">
                     < {{ __('Back') }}
                 </a>
+
+                <script>
+                    const paymentElements = document.getElementById('payment-elements');
+                    const cardholderName = document.getElementById('cardholder-name');
+                    const cardButton = document.getElementById('card-button');
+                    const message = document.getElementById('message');
+
+                    const stripe = Stripe('{{ $stripeKey }}');
+                    const elements = stripe.elements();
+                    const cardElement = elements.create('card');
+                    cardElement.mount('#card-element');
+
+                    cardButton.addEventListener('click', function() {
+                        stripe.handleCardPayment(
+                            '{{ $payment->clientSecret() }}', cardElement, {
+                                payment_method_data: {
+                                    billing_details: { name: cardholderName.value }
+                                }
+                            }
+                        ).then(function (result) {
+                            console.log(result);
+
+                            if (result.error) {
+                                if (result.error.code === 'parameter_invalid_empty' &&
+                                    result.error.param === 'payment_method_data[billing_details][name]') {
+                                    message.innerText = '⚠️ {{ __('Please provide your name.') }}';
+                                } else {
+                                    message.innerText = '⚠️ '+result.error.message;
+                                }
+
+                                message.classList.add('bg-red-400');
+                            } else {
+                                paymentElements.style.display = 'none';
+
+                                message.innerText = '✅ {{ __('The payment was successful.') }}';
+                                message.classList.add('bg-green-400');
+                            }
+
+                            message.style.display = 'block';
+                        });
+                    });
+                </script>
+            </div>
+
+            <p class="text-center text-gray-400 text-sm">
+                © {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
             </p>
         </div>
     </div>
