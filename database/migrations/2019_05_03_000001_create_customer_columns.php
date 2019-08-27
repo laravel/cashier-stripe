b9a143beb3c6a379656f5570946a13df24cafd47
Dries Vints <dries.vints@gmail.com>
2019-08-27 14:11:31 +0200

    Remove collation from migrations
    The reason for this is that the current collation is MySQL specific and doesn't works in other databases (like Postgres). This is still recommended by Stripe but maybe a note in the docs for this will suffice.
    
    Fixes https://github.com/laravel/cashier/issues/747

--- database/migrations/2019_05_03_000001_create_customer_columns.php
+++ database/migrations/2019_05_03_000001_create_customer_columns.php
@@ -14,7 +14,7 @@ class CreateCustomerColumns extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('stripe_id')->nullable()->collation('utf8mb4_bin')->index();
+            $table->string('stripe_id')->nullable()->index();
             $table->string('card_brand')->nullable();
             $table->string('card_last_four', 4)->nullable();
             $table->timestamp('trial_ends_at')->nullable();
