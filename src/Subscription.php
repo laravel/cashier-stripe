3fec26ead876a2aa2cfdd4ef3411319cd80c7c9c
Taylor Otwell <taylorotwell@gmail.com>
2020-04-13 14:56:52 -0500

    import class

--- src/Subscription.php
+++ src/Subscription.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Carbon\CarbonInterface;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use InvalidArgumentException;
 use Laravel\Cashier\Concerns\Prorates;
 use Laravel\Cashier\Exceptions\IncompletePayment;
