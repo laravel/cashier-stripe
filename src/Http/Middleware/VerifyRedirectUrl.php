f6841e6238b0c2ff037b94a5ca4c70e0a449ccd8
Dries Vints <dries@vints.io>
2020-05-04 17:42:57 +0200

    Protect against host mismatch

---
+++ src/Http/Middleware/VerifyRedirectUrl.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laravel\Cashier\Http\Middleware;
+
+use Closure;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class VerifyRedirectUrl
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+     */
+    public function handle($request, Closure $next)
+    {
+        $redirect = $request->get('redirect');
+
+        if (parse_url($redirect)['host'] !== $request->getHost()) {
+            throw new AccessDeniedHttpException('Redirect host mismatch.');
+        }
+
+        return $next($request);
+    }
+}
