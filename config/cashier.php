cc4069c5546803a09b4d3b5981701fc00ff2e91e
NanoCell Web Design <NanoCellWebDesign@users.noreply.github.com>
2020-09-28 17:21:18 +0100

    Updated default model to Laravel 8 Model namespace

--- config/cashier.php
+++ config/cashier.php
@@ -57,7 +57,7 @@ return [
     |
     */
 
-    'model' => env('CASHIER_MODEL', App\User::class),
+    'model' => env('CASHIER_MODEL', App\Models\User::class),
 
     /*
     |--------------------------------------------------------------------------
