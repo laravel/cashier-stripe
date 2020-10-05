0d3b1248199b77dff12f1230a5b8fa696382f094
Dries Vints <dries@vints.io>
2020-10-05 18:00:17 +0200

    Allow for custom styling

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,7 +1,8 @@
 <button
     id="checkout-{{ $sessionId }}"
     role="link"
-    style="background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+    style="{{ isset($style) && ! isset($class) ? $style : 'background-color:#6772E5;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em' }}"
+    @isset($class) class="{{ $class }}" @endisset
 >
     {{ $label }}
 </button>
