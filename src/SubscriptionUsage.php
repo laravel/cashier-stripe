6730e61af487bf7fe59ca5d3b59b919cab9d709d
Ben Doe <ben@reviews.io>
2020-12-16 15:10:39 +0000

    subscription usage model

---
+++ src/SubscriptionUsage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionUsage extends Model
+{
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
