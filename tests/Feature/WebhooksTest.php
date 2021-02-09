76e3273970acf4f7503eb188fa834933016cb407
Dries Vints <dries@vints.io>
2021-02-09 16:18:02 +0100

    Re-add test

--- tests/Feature/WebhooksTest.php
+++ tests/Feature/WebhooksTest.php
@@ -94,6 +94,62 @@ class WebhooksTest extends FeatureTestCase
         ]);
     }
 
+    public function test_subscriptions_are_updated()
+    {
+        $user = $this->createCustomer('subscriptions_are_updated', ['stripe_id' => 'cus_foo']);
+
+        $subscription = $user->subscriptions()->create([
+            'name' => 'main',
+            'stripe_id' => 'sub_foo',
+            'stripe_plan' => 'plan_foo',
+            'stripe_status' => Subscription::STATUS_ACTIVE,
+        ]);
+
+        $item = $subscription->items()->create([
+            'stripe_id' => 'it_foo',
+            'stripe_plan' => 'plan_bar',
+            'quantity' => 1,
+        ]);
+
+        $this->postJson('stripe/webhook', [
+            'id' => 'foo',
+            'type' => 'customer.subscription.updated',
+            'data' => [
+                'object' => [
+                    'id' => $subscription->stripe_id,
+                    'customer' => 'cus_foo',
+                    'cancel_at_period_end' => false,
+                    'quantity' => 5,
+                    'items' => [
+                        'data' => [[
+                            'id' => 'bar',
+                            'plan' => ['id' => 'plan_foo'],
+                            'quantity' => 10,
+                        ]],
+                    ],
+                ],
+            ],
+        ])->assertOk();
+
+        $this->assertDatabaseHas('subscriptions', [
+            'id' => $subscription->id,
+            'user_id' => $user->id,
+            'stripe_id' => 'sub_foo',
+            'quantity' => 5,
+        ]);
+
+        $this->assertDatabaseHas('subscription_items', [
+            'subscription_id' => $subscription->id,
+            'stripe_id' => 'bar',
+            'stripe_plan' => 'plan_foo',
+            'quantity' => 10,
+        ]);
+
+        $this->assertDatabaseMissing('subscription_items', [
+            'id' => $item->id,
+        ]);
+    }
+
     public function test_cancelled_subscription_is_properly_reactivated()
     {
         $user = $this->createCustomer('cancelled_subscription_is_properly_reactivated');
