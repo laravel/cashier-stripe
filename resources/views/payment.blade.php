9da4b0e1c797fb4a7409d168d73199e4c5a8a650
Dries Vints <dries@vints.io>
2021-03-05 15:05:55 +0100

    [12.x] Allow relative urls for redirect (#1082)
    * Allow relative urls for redirect
    
    * Use ubuntu-18.04

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -108,7 +108,7 @@
                     </div>
                 @endif
 
-                <button @click="goBack" ref="goBackButton" data-redirect="{{ $redirect ?? url('/') }}"
+                <button @click="goBack" ref="goBackButton" data-redirect="{{ $redirect }}"
                    class="inline-block w-full px-4 py-3 bg-gray-200 hover:bg-gray-300 text-center text-gray-700 rounded-lg">
                     {{ __('Go back') }}
                 </button>
--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -24,7 +24,7 @@ class PaymentController extends Controller
      * Display the form to gather additional payment verification for the given payment.
      *
      * @param  string  $id
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Contracts\View\View
      */
     public function show($id)
     {
@@ -33,7 +33,7 @@ class PaymentController extends Controller
             'payment' => new Payment(
                 StripePaymentIntent::retrieve($id, Cashier::stripeOptions())
             ),
-            'redirect' => request('redirect'),
+            'redirect' => url(request('redirect', '/')),
         ]);
     }
 }
