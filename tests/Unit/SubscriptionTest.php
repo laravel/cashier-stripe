5becdb439cd8318255997de4de0755ac0755a675
Sonja Turo <sonja.turo@rout.net>
2021-03-13 20:36:19 +1100

    Updated  tests to use factories

--- tests/Unit/SubscriptionTest.php
+++ tests/Unit/SubscriptionTest.php
@@ -5,16 +5,13 @@ namespace Laravel\Cashier\Tests\Unit;
 use InvalidArgumentException;
 use Laravel\Cashier\Exceptions\SubscriptionUpdateFailure;
 use Laravel\Cashier\Subscription;
-use PHPUnit\Framework\TestCase;
-use Stripe\Subscription as StripeSubscription;
+use Laravel\Cashier\Tests\TestCase;
 
 class SubscriptionTest extends TestCase
 {
     public function test_we_can_check_if_a_subscription_is_incomplete()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_INCOMPLETE,
-        ]);
+        $subscription = Subscription::factory()->incomplete()->make();
 
         $this->assertTrue($subscription->incomplete());
         $this->assertFalse($subscription->pastDue());
@@ -23,9 +20,7 @@ class SubscriptionTest extends TestCase
 
     public function test_we_can_check_if_a_subscription_is_past_due()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_PAST_DUE,
-        ]);
+        $subscription = Subscription::factory()->pastDue()->make();
 
         $this->assertFalse($subscription->incomplete());
         $this->assertTrue($subscription->pastDue());
@@ -34,9 +29,7 @@ class SubscriptionTest extends TestCase
 
     public function test_we_can_check_if_a_subscription_is_active()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_ACTIVE,
-        ]);
+        $subscription = Subscription::factory()->active()->make();
 
         $this->assertFalse($subscription->incomplete());
         $this->assertFalse($subscription->pastDue());
@@ -45,63 +38,49 @@ class SubscriptionTest extends TestCase
 
     public function test_an_incomplete_subscription_is_not_valid()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_INCOMPLETE,
-        ]);
+        $subscription = Subscription::factory()->incomplete()->make();
 
         $this->assertFalse($subscription->valid());
     }
 
     public function test_a_past_due_subscription_is_not_valid()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_PAST_DUE,
-        ]);
+        $subscription = Subscription::factory()->pastDue()->make();
 
         $this->assertFalse($subscription->valid());
     }
 
     public function test_an_active_subscription_is_valid()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_ACTIVE,
-        ]);
+        $subscription = Subscription::factory()->active()->make();
 
         $this->assertTrue($subscription->valid());
     }
 
     public function test_payment_is_incomplete_when_status_is_incomplete()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_INCOMPLETE,
-        ]);
+        $subscription = Subscription::factory()->incomplete()->make();
 
         $this->assertTrue($subscription->hasIncompletePayment());
     }
 
     public function test_payment_is_incomplete_when_status_is_past_due()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_PAST_DUE,
-        ]);
+        $subscription = Subscription::factory()->pastDue()->make();
 
         $this->assertTrue($subscription->hasIncompletePayment());
     }
 
     public function test_payment_is_not_incomplete_when_status_is_active()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_ACTIVE,
-        ]);
+        $subscription = Subscription::factory()->active()->make();
 
         $this->assertFalse($subscription->hasIncompletePayment());
     }
 
     public function test_incomplete_subscriptions_cannot_be_swapped()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_INCOMPLETE,
-        ]);
+        $subscription = Subscription::factory()->incomplete()->make();
 
         $this->expectException(SubscriptionUpdateFailure::class);
 
@@ -110,9 +89,7 @@ class SubscriptionTest extends TestCase
 
     public function test_incomplete_subscriptions_cannot_update_their_quantity()
     {
-        $subscription = new Subscription([
-            'stripe_status' => StripeSubscription::STATUS_INCOMPLETE,
-        ]);
+        $subscription = Subscription::factory()->incomplete()->make();
 
         $this->expectException(SubscriptionUpdateFailure::class);
 
@@ -128,15 +105,15 @@ class SubscriptionTest extends TestCase
 
     public function test_we_can_check_if_it_has_a_single_plan()
     {
-        $subscription = new Subscription(['stripe_plan' => 'foo']);
-
+        $subscription = Subscription::factory()->withPlan('foo')->make();
+        
         $this->assertTrue($subscription->hasSinglePlan());
         $this->assertFalse($subscription->hasMultiplePlans());
     }
 
     public function test_we_can_check_if_it_has_multiple_plans()
     {
-        $subscription = new Subscription(['stripe_plan' => null]);
+        $subscription = Subscription::factory()->withPlan(null)->make();
 
         $this->assertTrue($subscription->hasMultiplePlans());
         $this->assertFalse($subscription->hasSinglePlan());
