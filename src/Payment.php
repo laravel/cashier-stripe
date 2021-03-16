86be4a463e0e60b2fbf37c96e6ebc0d2eb384c0b
Dries Vints <dries@vints.io>
2021-03-16 15:01:31 +0100

    [12.x] Add capture method (#1099)
    * Add capture method
    
    * Update Payment.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Payment.php
+++ src/Payment.php
@@ -57,6 +57,17 @@ class Payment
     }
 
     /**
+     * Capture a payment that is being held for the customer.
+     *
+     * @param  array  $options
+     * @return \Stripe\PaymentIntent
+     */
+    public function capture(array $options = [])
+    {
+        return $this->paymentIntent->capture($options, Cashier::stripeOptions());
+    }
+
+    /**
      * Determine if the payment needs a valid payment method.
      *
      * @return bool
