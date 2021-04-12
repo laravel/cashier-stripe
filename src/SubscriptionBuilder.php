60aec3858a1a0c1c4348a7d7edbb7d9af381cf41
Dries Vints <dries@vints.io>
2021-04-12 11:49:18 +0200

    Extract creating subscription to a separate method

--- src/SubscriptionBuilder.php
+++ src/SubscriptionBuilder.php
@@ -310,6 +310,25 @@ class SubscriptionBuilder
             $this->owner->stripeOptions()
         );
 
+        $subscription = $this->createSubscription($stripeSubscription);
+
+        if ($subscription->hasIncompletePayment()) {
+            (new Payment(
+                $stripeSubscription->latest_invoice->payment_intent
+            ))->validate();
+        }
+
+        return $subscription;
+    }
+
+    /**
+     * Create the Eloquent Subscription.
+     *
+     * @param  \Stripe\Subscription  $stripeSubscription
+     * @return \Laravel\Cashier\Subscription
+     */
+    protected function createSubscription(StripeSubscription $stripeSubscription)
+    {
         /** @var \Stripe\SubscriptionItem $firstItem */
         $firstItem = $stripeSubscription->items->first();
         $isSinglePlan = $stripeSubscription->items->count() === 1;
@@ -321,7 +340,7 @@ class SubscriptionBuilder
             'stripe_status' => $stripeSubscription->status,
             'stripe_plan' => $isSinglePlan ? $firstItem->plan->id : null,
             'quantity' => $isSinglePlan ? $firstItem->quantity : null,
-            'trial_ends_at' => ! $this->skipTrial ? $this->trialExpires : null,
+            'trial_ends_at' => !$this->skipTrial ? $this->trialExpires : null,
             'ends_at' => null,
         ]);
 
@@ -332,12 +351,6 @@ class SubscriptionBuilder
                 'stripe_plan' => $item->plan->id,
                 'quantity' => $item->quantity,
             ]);
-        }
-
-        if ($subscription->hasIncompletePayment()) {
-            (new Payment(
-                $stripeSubscription->latest_invoice->payment_intent
-            ))->validate();
         }
 
         return $subscription;
