2aa2e2bf625e40ea31c5ae597693b6fc2da2d166
Dries Vints <dries@vints.io>
2021-05-17 15:31:43 +0200

    Prorations and extending trials (#1151)

--- src/Subscription.php
+++ src/Subscription.php
@@ -587,11 +587,10 @@ class Subscription extends Model
             return $this;
         }
 
-        $subscription = $this->asStripeSubscription();
-
-        $subscription->trial_end = 'now';
-
-        $subscription->save();
+        $this->updateStripeSubscription([
+            'trial_end' => 'now',
+            'proration_behavior' => $this->prorateBehavior(),
+        ]);
 
         $this->trial_ends_at = null;
 
@@ -612,11 +611,10 @@ class Subscription extends Model
             throw new InvalidArgumentException("Extending a subscription's trial requires a date in the future.");
         }
 
-        $subscription = $this->asStripeSubscription();
-
-        $subscription->trial_end = $date->getTimestamp();
-
-        $subscription->save();
+        $this->updateStripeSubscription([
+            'trial_end' => $date->getTimestamp(),
+            'proration_behavior' => $this->prorateBehavior(),
+        ]);
 
         $this->trial_ends_at = $date;
 
