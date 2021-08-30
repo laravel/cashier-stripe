01f82b155f22a49b0d73e81f96b415a420bc9bf9
Dries Vints <dries@vints.io>
2021-08-30 15:59:34 +0200

    [13.x] Add invoices methods to subscription (#1245)
    * Add invoices methods to subscription
    
    * Update ManagesInvoices.php
    
    * Update Subscription.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Concerns/ManagesInvoices.php
+++ src/Concerns/ManagesInvoices.php
@@ -270,7 +270,7 @@ trait ManagesInvoices
     }
 
     /**
-     * Get an array of the customer's invoices.
+     * Get an array of the customer's invoices, including pending invoices.
      *
      * @param  array  $parameters
      * @return \Illuminate\Support\Collection|\Laravel\Cashier\Invoice[]
