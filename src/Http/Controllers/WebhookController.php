2fc19633d55e9c0c3eb6cf0404cbdd76ccf7a40d
Dries Vints <dries@vints.io>
2021-08-30 16:01:53 +0200

    [13.x] Fix webhook order issue (#1243)
    * Create or update subscription
    
    * Add unique indexes
    
    * Fix tests

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -120,83 +120,78 @@ class WebhookController extends Controller
      */
     protected function handleCustomerSubscriptionUpdated(array $payload)
     {
-        // Prevent issue with subscription created webhook arriving too late...
-        sleep(1);
-
         if ($user = $this->getUserByStripeId($payload['data']['object']['customer'])) {
             $data = $payload['data']['object'];
 
-            $user->subscriptions->filter(function (Subscription $subscription) use ($data) {
-                return $subscription->stripe_id === $data['id'];
-            })->each(function (Subscription $subscription) use ($data) {
-                if (
-                    isset($data['status']) &&
-                    $data['status'] === StripeSubscription::STATUS_INCOMPLETE_EXPIRED
-                ) {
-                    $subscription->items()->delete();
-                    $subscription->delete();
+            $subscription = $user->subscriptions()->firstOrNew(['stripe_id' => $data['id']]);
 
-                    return;
+            if (
+                isset($data['status']) &&
+                $data['status'] === StripeSubscription::STATUS_INCOMPLETE_EXPIRED
+            ) {
+                $subscription->items()->delete();
+                $subscription->delete();
+
+                return;
+            }
+
+            $firstItem = $data['items']['data'][0];
+            $isSinglePrice = count($data['items']['data']) === 1;
+
+            // Price...
+            $subscription->stripe_price = $isSinglePrice ? $firstItem['price']['id'] : null;
+
+            // Quantity...
+            $subscription->quantity = $isSinglePrice && isset($firstItem['quantity']) ? $firstItem['quantity'] : null;
+
+            // Trial ending date...
+            if (isset($data['trial_end'])) {
+                $trialEnd = Carbon::createFromTimestamp($data['trial_end']);
+
+                if (! $subscription->trial_ends_at || $subscription->trial_ends_at->ne($trialEnd)) {
+                    $subscription->trial_ends_at = $trialEnd;
+                }
+            }
+
+            // Cancellation date...
+            if (isset($data['cancel_at_period_end'])) {
+                if ($data['cancel_at_period_end']) {
+                    $subscription->ends_at = $subscription->onTrial()
+                        ? $subscription->trial_ends_at
+                        : Carbon::createFromTimestamp($data['current_period_end']);
+                } elseif (isset($data['cancel_at'])) {
+                    $subscription->ends_at = Carbon::createFromTimestamp($data['cancel_at']);
+                } else {
+                    $subscription->ends_at = null;
+                }
+            }
+
+            // Status...
+            if (isset($data['status'])) {
+                $subscription->stripe_status = $data['status'];
+            }
+
+            $subscription->save();
+
+            // Update subscription items...
+            if (isset($data['items'])) {
+                $prices = [];
+
+                foreach ($data['items']['data'] as $item) {
+                    $prices[] = $item['price']['id'];
+
+                    $subscription->items()->updateOrCreate([
+                        'stripe_id' => $item['id'],
+                    ], [
+                        'stripe_product' => $item['price']['product'],
+                        'stripe_price' => $item['price']['id'],
+                        'quantity' => $item['quantity'] ?? null,
+                    ]);
                 }
 
-                $firstItem = $data['items']['data'][0];
-                $isSinglePrice = count($data['items']['data']) === 1;
-
-                // Price...
-                $subscription->stripe_price = $isSinglePrice ? $firstItem['price']['id'] : null;
-
-                // Quantity...
-                $subscription->quantity = $isSinglePrice && isset($firstItem['quantity']) ? $firstItem['quantity'] : null;
-
-                // Trial ending date...
-                if (isset($data['trial_end'])) {
-                    $trialEnd = Carbon::createFromTimestamp($data['trial_end']);
-
-                    if (! $subscription->trial_ends_at || $subscription->trial_ends_at->ne($trialEnd)) {
-                        $subscription->trial_ends_at = $trialEnd;
-                    }
-                }
-
-                // Cancellation date...
-                if (isset($data['cancel_at_period_end'])) {
-                    if ($data['cancel_at_period_end']) {
-                        $subscription->ends_at = $subscription->onTrial()
-                            ? $subscription->trial_ends_at
-                            : Carbon::createFromTimestamp($data['current_period_end']);
-                    } elseif (isset($data['cancel_at'])) {
-                        $subscription->ends_at = Carbon::createFromTimestamp($data['cancel_at']);
-                    } else {
-                        $subscription->ends_at = null;
-                    }
-                }
-
-                // Status...
-                if (isset($data['status'])) {
-                    $subscription->stripe_status = $data['status'];
-                }
-
-                $subscription->save();
-
-                // Update subscription items...
-                if (isset($data['items'])) {
-                    $prices = [];
-
-                    foreach ($data['items']['data'] as $item) {
-                        $prices[] = $item['price']['id'];
-
-                        $subscription->items()->updateOrCreate([
-                            'stripe_id' => $item['id'],
-                        ], [
-                            'stripe_product' => $item['price']['product'],
-                            'stripe_price' => $item['price']['id'],
-                            'quantity' => $item['quantity'] ?? null,
-                        ]);
-                    }
-
-                    // Delete items that aren't attached to the subscription anymore...
-                    $subscription->items()->whereNotIn('stripe_price', $prices)->delete();
-                }
-            });
+                // Delete items that aren't attached to the subscription anymore...
+                $subscription->items()->whereNotIn('stripe_price', $prices)->delete();
+            }
         }
 
         return $this->successMethod();
