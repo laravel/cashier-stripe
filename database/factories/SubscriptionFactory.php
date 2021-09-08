42443c38c6d799b8773fd4052b20108ef171e911
Dries Vints <dries@vints.io>
2021-09-08 10:50:24 +0200

    Update SubscriptionFactory.php

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -29,7 +29,7 @@ class SubscriptionFactory extends Factory
 
         return [
             (new $model)->getForeignKey() => ($model)::factory(),
-            'name' => $this->faker->title,
+            'name' => 'default',
             'stripe_id' => 'sub_'.Str::random(40),
             'stripe_status' => StripeSubscription::STATUS_ACTIVE,
             'stripe_price' => null,
