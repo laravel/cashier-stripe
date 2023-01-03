301d5a07e8a90c41cdf2bcdf8688680adef4d007
Nuno Maduro <enunomaduro@gmail.com>
2023-01-03 09:39:34 +0000

    Adds types to database migrations and factories (#1472)

--- database/migrations/2019_05_03_000001_create_customer_columns.php
+++ database/migrations/2019_05_03_000001_create_customer_columns.php
@@ -8,10 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('stripe_id')->nullable()->index();
@@ -23,10 +21,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn([
