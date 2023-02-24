56d28851d27ede649dc199ce71e9d485c7716d03
Dries Vints <dries@vints.be>
2023-02-24 11:21:24 +0100

    Dates

--- src/Subscription.php
+++ src/Subscription.php
@@ -49,18 +49,8 @@ class Subscription extends Model
      */
     protected $casts = [
         'quantity' => 'integer',
-    ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'created_at',
-        'ends_at',
-        'trial_ends_at',
-        'updated_at',
+        'ends_at' => 'datetime',
+        'trial_ends_at' => 'datetime',
     ];
 
     /**
