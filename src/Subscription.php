934533e2eb32da9b13b47647e1e0aa8c4e589380
Dries Vints <dries@vints.be>
2023-11-02 08:33:59 +0100

    Better cleanup for subscription items

--- src/Subscription.php
+++ src/Subscription.php
@@ -699,10 +699,10 @@ class Subscription extends Model
             'ends_at' => null,
         ])->save();
 
-        $stripePrices = [];
+        $cleanup = [];
 
         foreach ($stripeSubscription->items as $item) {
-            $stripePrices[] = $item->price->id;
+            $cleanup[] = $item->id;
 
             $this->items()->updateOrCreate([
                 'stripe_id' => $item->id,
@@ -714,7 +714,7 @@ class Subscription extends Model
         }
 
         // Delete items that aren't attached to the subscription anymore...
-        $this->items()->whereNotIn('stripe_price', $stripePrices)->delete();
+        $this->items()->whereNotIn('stripe_id', $cleanup)->delete();
 
         $this->unsetRelation('items');
 
