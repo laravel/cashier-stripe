675f4514978f9fd9e69723035ab3e771e0dc1c11
Arafat Islam <arafatkn@gmail.com>
2024-02-29 14:44:24 +0600

    create stripe customer or if exists then update/sync

--- src/Concerns/ManagesCustomer.php
+++ src/Concerns/ManagesCustomer.php
@@ -128,6 +128,36 @@ trait ManagesCustomer
     }
 
     /**
+     * Update the Stripe customer information for the current user or create one.
+     *
+     * @param  array  $options
+     * @return \Stripe\Customer
+     */
+    public function createOrUpdateStripeCustomer(array $options = [])
+    {
+        if ($this->hasStripeId()) {
+            return $this->updateStripeCustomer($options);
+        }
+
+        return $this->createAsStripeCustomer($options);
+    }
+
+    /**
+     * Sync the customer's information to Stripe for the current user or create one.
+     *
+     * @param  array  $options
+     * @return \Stripe\Customer
+     */
+    public function createOrSyncStripeCustomer(array $options = [])
+    {
+        if ($this->hasStripeId()) {
+            return $this->syncStripeCustomerDetails();
+        }
+
+        return $this->createAsStripeCustomer($options);
+    }
+
+    /**
      * Get the Stripe customer for the model.
      *
      * @param  array  $expand
