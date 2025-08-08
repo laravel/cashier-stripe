e272a3bbf72c2376607a71e2218d0e81329ba2a8
Taylor Otwell <taylor@laravel.com>
2025-08-08 09:04:33 -0500

    Update Checkout.php

--- src/Checkout.php
+++ src/Checkout.php
@@ -84,11 +84,10 @@ class Checkout implements Arrayable, Jsonable, JsonSerializable, Responsable
 
         // Make sure to collect address and name when Tax ID collection is enabled...
         if (isset($data['customer']) && ($data['tax_id_collection']['enabled'] ?? false)) {
-            // Use billing_address_collection instead of customer_update.address = 'auto' to
-            // prevent address lock in checkout after payment cancellation while allowing overrides
             if (! isset($data['billing_address_collection'])) {
                 $data['billing_address_collection'] = 'required';
             }
+
             $data['customer_update']['name'] = 'auto';
         }
 
