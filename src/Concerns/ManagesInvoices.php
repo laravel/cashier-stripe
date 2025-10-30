5d54f4c252433dd948d226c2bb4f4b645dd65b88
Josias Montag <josias@montag.info>
2025-10-30 09:54:49 +0100

    Update ManagesInvoices.php

--- src/Concerns/ManagesInvoices.php
+++ src/Concerns/ManagesInvoices.php
@@ -152,7 +152,7 @@ trait ManagesInvoices
             return $invoice->chargesAutomatically() ? $invoice->pay($payOptions) : $invoice->send();
         } catch (StripeCardException $exception) {
             // Get the latest payment from the invoice payments...
-            $stripeInvoice = $invoice->refresh(['payments']);
+            $stripeInvoice = $invoice->refresh(['payments'])->asStripeInvoice();
 
             $invoicePayments = $stripeInvoice->payments->data;
 
