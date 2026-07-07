e0d95f0989b950b60e470a12f33625ff3bc7a4bc
Mior Muhammad Zaki <crynobone@gmail.com>
2026-07-07 22:08:48 +0800

    Update Checkout.php

--- src/Checkout.php
+++ src/Checkout.php
@@ -99,7 +99,7 @@ class Checkout implements Arrayable, Jsonable, JsonSerializable, Responsable
             $data['cancel_url'] = $sessionOptions['cancel_url'] ?? route('home').'?checkout=cancelled';
         }
 
-        if (isset($data['ui_mode']) && in_array(StripeApiVersion::CURRENT_MAJOR, ['dahlia', 'clover'])) {
+        if (isset($data['ui_mode']) && in_array(StripeApiVersion::CURRENT_MAJOR, ['dahlia'])) {
             $data['ui_mode'] = match ($data['ui_mode']) {
                 'embedded' => 'embedded_page',
                 'custom' => 'elements',
